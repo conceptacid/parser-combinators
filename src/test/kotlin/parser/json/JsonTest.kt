85e9e65eecb8a6fe8935e15c657c57d6da98779b
conceptacid <vasiliy.shmyrev@gmail.com>
2019-12-13 00:30:21 -0800

    (+) long string tests added

--- src/test/kotlin/parser/json/JsonTest.kt
+++ src/test/kotlin/parser/json/JsonTest.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import parser.core.State
 import parser.core.Success
+import java.util.*
 
 class JsonTest {
 
@@ -38,6 +39,38 @@ class JsonTest {
     }
 
     @Test
+    fun `very long string`() {
+        val chars = ('a'..'z').toList() + ('A'..'Z').toList()
+        val rand = Random()
+
+        val size = 6 * 1024 // 5 is ok, 6 is too big
+        val text = Array<Char>(size) { chars[rand.nextInt(chars.size)] }.joinToString("")
+        val input = "\"$text\""
+        val output = JString(text)
+        assertThat(json().run(State(input)))
+                .isEqualTo(Success(output, State(input = input, line = 0, col = input.length, pos = input.length)))
+    }
+
+    @Test
+    fun `many very long strings`() {
+        val chars = ('a'..'z').toList() + ('A'..'Z').toList()
+        val rand = Random()
+
+        val num = 500
+        val size = 1024
+        val textArray = Array<String>(num) {
+            Array<Char>(size) { chars[rand.nextInt(chars.size)] }.joinToString("")
+        }
+
+        val input = "[" + textArray.map { "\"$it\"" }.joinToString(",") + "]"
+        val output = JArray(textArray.map { JString(it) })
+
+        assertThat(json().run(State(input)))
+                .isEqualTo(Success(output, State(input = input, line = 0, col = input.length, pos = input.length)))
+
+    }
+
+    @Test
     fun `full json object`() {
         val input = """
             {
@@ -55,31 +88,31 @@ class JsonTest {
             }""".trimIndent()
 
         val output = JObject(
-            mapOf(
-                "name" to JString("Scott"),
-                "isMale" to JBool(true),
-                "bday" to JObject(
-                    mapOf(
-                        "year" to JNumber(2001),
-                        "month" to JNumber(12),
-                        "day" to JNumber(25)
-                    )
-                ),
-                "favouriteColors" to JArray(
-                    listOf(
-                        JString("blue"),
-                        JString("green")
-                    )
+                mapOf(
+                        "name" to JString("Scott"),
+                        "isMale" to JBool(true),
+                        "bday" to JObject(
+                                mapOf(
+                                        "year" to JNumber(2001),
+                                        "month" to JNumber(12),
+                                        "day" to JNumber(25)
+                                )
+                        ),
+                        "favouriteColors" to JArray(
+                                listOf(
+                                        JString("blue"),
+                                        JString("green")
+                                )
+                        )
                 )
-            )
         )
 
         assertThat(json().run(State(input)))
-            .isEqualTo(Success(output, State(input = input, line = 12, col = 1, pos = input.length)))
+                .isEqualTo(Success(output, State(input = input, line = 12, col = 1, pos = input.length)))
     }
 
     private infix fun String.shouldBe(value: JValue) {
         assertThat(json().run(State(this)))
-            .isEqualTo(Success(value, State(input = this, col = length, pos = length)))
+                .isEqualTo(Success(value, State(input = this, col = length, pos = length)))
     }
 }
